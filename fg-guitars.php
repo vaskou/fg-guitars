101cd2f277390ff4f18485eb0415c5071378804e
Vasilis Koutsopoulos <vaskou@hotmail.com>
2020-03-21 12:08:32 +0200

    -Init

---
+++ fg-guitars.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       FremeditiGuitars - Guitars
+ * Description:       FremeditiGuitars - Guitars Post Type
+ * Version:           1.0.0
+ * Author:            Vasilis Koutsopoulos
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       fg-guitars
+ * Domain Path:       /languages
+ */
+
+defined( 'ABSPATH' ) or die();
+
+define( 'FG_GUITARS_VERSION', '1.0.0' );
+define( 'FG_GUITARS_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'FG_GUITARS_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+define( 'FG_GUITARS_PLUGIN_DIR_NAME', basename( FG_GUITARS_PLUGIN_DIR_PATH ) );
+define( 'FG_GUITARS_PLUGIN_URL', plugins_url( FG_GUITARS_PLUGIN_DIR_NAME ) );
+
+
+

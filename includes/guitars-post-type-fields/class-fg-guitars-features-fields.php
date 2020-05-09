22574cb65500e8a1e0c1cc1cf2a3dd66972e203a
Vasilis Koutsopoulos <vaskou@hotmail.com>
2020-05-09 12:27:08 +0300

    -WIP

--- includes/guitars-post-type-fields/class-fg-guitars-features-fields.php
+++ includes/guitars-post-type-fields/class-fg-guitars-features-fields.php
@@ -13,20 +13,31 @@ class FG_Guitars_Features_Fields extends FG_Guitars_Post_Type_Fields {
 	}
 
 	private function __construct() {
-		if ( ! class_exists( 'FG_Features_CMB2_Field_Dropdown' ) ) {
+		if ( ! class_exists( 'FG_Features_CMB2_Field_Dropdown' ) && ! class_exists( 'FG_Pickups_CMB2_Field_Dropdown' ) ) {
 			$this->enabled = false;
+
 			return;
 		}
 
 		$this->metabox_id    = 'features';
 		$this->metabox_title = __( 'Features', 'fg-guitars' );
-		$this->fields        = array(
-			'feature' => array(
+		$this->fields        = array();
+
+		if ( class_exists( 'FG_Features_CMB2_Field_Dropdown' ) ) {
+			$this->fields['feature'] = array(
 				'name'       => __( 'Feature', 'fg-guitars' ),
 				'type'       => 'fg_features_cmb2_field_dropdown',
 				'repeatable' => true,
-			),
-		);
+			);
+		}
+
+		if ( class_exists( 'FG_Pickups_CMB2_Field_Dropdown' ) ) {
+			$this->fields['pickup'] = array(
+				'name'       => __( 'Pickup', 'fg-guitars' ),
+				'type'       => 'fg_pickups_cmb2_field_dropdown',
+				'repeatable' => true,
+			);
+		}
 	}
 
 }

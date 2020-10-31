dd1099777c0f171edb24f0f6e87ef0e3144e18a3
vaskou <vaskou@hotmail.com>
2020-10-31 11:22:10 +0200

    -WIP

--- fg-guitars.php
+++ fg-guitars.php
@@ -4,7 +4,7 @@
  * @wordpress-plugin
  * Plugin Name:       FremeditiGuitars - Guitars
  * Description:       FremeditiGuitars - Guitars Post Type
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Vasilis Koutsopoulos
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
@@ -14,7 +14,7 @@
 
 defined( 'ABSPATH' ) or die();
 
-define( 'FG_GUITARS_VERSION', '1.0.0' );
+define( 'FG_GUITARS_VERSION', '1.0.1' );
 define( 'FG_GUITARS_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'FG_GUITARS_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 define( 'FG_GUITARS_PLUGIN_DIR_NAME', basename( FG_GUITARS_PLUGIN_DIR_PATH ) );
